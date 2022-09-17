b1789ab7dbda94320ce2198426793d0c1dc98156
ɐʇɯon3ɹ <contact.atmoner@gmail.com>
2022-09-17 13:53:53 +0200

    Update bitcanna-widget.php

--- bitcanna-widget.php
+++ bitcanna-widget.php
@@ -18,7 +18,7 @@ if ( ! defined( 'WPINC' ) ) {
 * Start at version 1.0.0 and use SemVer - https://semver.org
 * Rename this for your plugin and update it as you release new versions.
 */
-define( 'BITCANNA_WIDGET', '1.0.1' );
+define( 'BITCANNA_WIDGET', '1.1.1' );
 
 // The widget class
 class Bitcanna_Widget extends WP_Widget {
